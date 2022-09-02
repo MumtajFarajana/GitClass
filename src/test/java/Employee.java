2c330d9e7e7772e0f187797b5de1bfd92d0e2856
MumtajFarajana <farajana01@gmail.com>
2022-09-02 22:56:08 +0530

    init employee

---
+++ src/test/java/Employee.java
@@ -0,0 +1,21 @@
+
+public class Employee {
+	
+	public void name1() {
+		System.out.println("name1");
+	}
+	public void name2() {
+		System.out.println("name2");
+	}
+	public void name3() {
+		System.out.println("name3");
+	}
+public static void main(String[] args) {
+	
+
+	Employee e=new Employee();
+	e.name1();
+	e.name2();
+	e.name3();
+}
+}
