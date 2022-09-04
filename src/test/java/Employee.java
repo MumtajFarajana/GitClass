bd4d1531e72f79728baa6a517ca5be9125850029
Mumtaj Farajana <farajana01@gmail.com>
2022-09-04 20:31:05 +0530

    added code farajana

--- src/test/java/Employee.java
+++ src/test/java/Employee.java
@@ -10,6 +10,15 @@ public class Employee {
 	public void name3() {
 		System.out.println("name3");
 	}
+	public void name4() {
+		System.out.println("name4");
+	}
+	public void name5() {
+		System.out.println("name5");
+	}
+	public void name6() {
+		System.out.println("name6");
+	}
 public static void main(String[] args) {
 	
 
@@ -17,5 +26,8 @@ public static void main(String[] args) {
 	e.name1();
 	e.name2();
 	e.name3();
+	e.name4();
+	e.name5();
+	e.name6();
 }
 }
